409215657719d1951582f6f9da165a432aabb814
1234 <12345@gmail.com>
2021-09-07 16:51:17 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radiustextbox = new System.Windows.Forms.TextBox();
+            this.plotnosttextbox = new System.Windows.Forms.TextBox();
+            this.massatextbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.otvetlabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radiustextbox);
+            this.groupBox1.Controls.Add(this.plotnosttextbox);
+            this.groupBox1.Controls.Add(this.massatextbox);
+            this.groupBox1.Location = new System.Drawing.Point(31, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(726, 136);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "inputdata";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(280, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(427, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Радиус";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(220, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Плотность";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Масса";
+            // 
+            // radiustextbox
+            // 
+            this.radiustextbox.Location = new System.Drawing.Point(430, 41);
+            this.radiustextbox.Name = "radiustextbox";
+            this.radiustextbox.Size = new System.Drawing.Size(179, 20);
+            this.radiustextbox.TabIndex = 2;
+            // 
+            // plotnosttextbox
+            // 
+            this.plotnosttextbox.Location = new System.Drawing.Point(223, 41);
+            this.plotnosttextbox.Name = "plotnosttextbox";
+            this.plotnosttextbox.Size = new System.Drawing.Size(179, 20);
+            this.plotnosttextbox.TabIndex = 1;
+            // 
+            // massatextbox
+            // 
+            this.massatextbox.Location = new System.Drawing.Point(6, 41);
+            this.massatextbox.Name = "massatextbox";
+            this.massatextbox.Size = new System.Drawing.Size(179, 20);
+            this.massatextbox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(232, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(366, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Получить ответ!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label4.Location = new System.Drawing.Point(3, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(393, 108);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "ОТВЕТ:";
+            // 
+            // otvetlabel
+            // 
+            this.otvetlabel.AutoSize = true;
+            this.otvetlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.otvetlabel.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.otvetlabel.Location = new System.Drawing.Point(411, 203);
+            this.otvetlabel.Name = "otvetlabel";
+            this.otvetlabel.Size = new System.Drawing.Size(0, 108);
+            this.otvetlabel.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "гр.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(220, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "кг/см^3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(427, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "см.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.otvetlabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox radiustextbox;
+        private System.Windows.Forms.TextBox plotnosttextbox;
+        private System.Windows.Forms.TextBox massatextbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label otvetlabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void reshenie(double m, double p, double r)
+        {
+            label5.Text = "";
+            double otvet;
+            otvet = Math.Round((m / p) / (Math.PI * (r * r)),3);
+            string otv = Convert.ToString(otvet);
+            otvetlabel.Text = otv;
+            massatextbox.Text = "";
+            plotnosttextbox.Text = "";
+            radiustextbox.Text = "";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double m, p, r;
+            if (massatextbox.Text == "" || plotnosttextbox.Text == "" || radiustextbox.Text == "")
+                label5.Text = "Введите значения!";
+            else
+            {
+                m = Convert.ToDouble(massatextbox.Text);
+                p = Convert.ToDouble(plotnosttextbox.Text);
+                r = Convert.ToDouble(radiustextbox.Text);
+                reshenie(m, p, r);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            massatextbox.Text = "";
+            plotnosttextbox.Text = "";
+            radiustextbox.Text = "";
+        }
+    }
+}
